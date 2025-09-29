08625ffa09992895a7ab35bf8605e5549b4cbca0
Abigail Santiago <abigail.santiago@northwestschool.org>
2025-09-29 09:41:23 -0700

    Update and rename calculator.cpp to parens.cpp

---
+++ parens.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <string>
+using namespace std;
+bool isBalanced(string expression) {
+ vector<char> stack;
+ 
+ for (char c : expression) {
+  if (c == '(') {
+   //Task: what do we do?
+  }
+  else if (c == ')') {
+   // Task: what do we do?
+  }
+  }
+  
+ // Task: how do we know if balanced?
+ return true;
+}
